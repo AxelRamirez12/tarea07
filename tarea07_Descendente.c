90c0773f3170ab5a7a058e750ffbadb0bebd796c
Axel <3020680740101@ingenieria.usac.edu.gt>
2024-03-05 22:52:58 -0600

    first commit

---
+++ tarea07_Descendente.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    float *pesos, temp;
+    int i,j,nest;
+    printf("Cuantos estudiantes son?:");
+    scanf("%d",&nest);
+    pesos = (float*) malloc(nest*sizeof (float));
+    if (pesos==NULL)
+    {   
+        printf("Insuficiente espacio de Memoria\n");
+        exit(-1); //Salir del Programa
+    }
+    for (i=0; i<nest; i++)
+        {
+            printf("Peso del Estudiante en libras [%d]:", i+1);
+            scanf("%f", (pesos+i));
+        }
+    printf("\n****ARRAY ORIGINAL****\n");
+    for (i=0;i<nest;i++)
+    printf("Peso[%d]: %.1f\n",i+1,*(pesos+i));
+    for (i=0;i<nest;i++)
+    for (j=0;j<(nest-1);j++)
+    if(pesos[j] < pesos[j+1])
+        {
+            temp=pesos[j];
+            pesos[j] = pesos[j+1];
+            pesos[j+1] = temp;
+        }
+    printf("\n****ARRAY ORDENADO EN FORMATO DESCENDENTE****\n");
+    for (i=0;i<nest;i++)
+    printf("Peso[%d]: %.1f\n",i+1,*(pesos+i));
+}
